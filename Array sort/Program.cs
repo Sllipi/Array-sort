3cce46682c493fec271ca1ba0c7f96847991f246
Sllipi <Sllipi@SLLIPI->
2022-04-29 00:02:28 +0300

    Добавьте файлы проекта.

---
+++ Array sort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Big_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            byte randMin = 1;
+            byte randMax = 50;
+            byte[] bigElement = new byte[10];
+            byte identicalElement = 0;
+            for (byte i = 0; i < bigElement.Length; i++)
+            {
+                bigElement[i] = (byte)rand.Next(randMin, randMax);
+                Console.Write(bigElement[i] + " ");
+                for (byte k = 0; k < bigElement.Length; k++)
+                {
+                    if (bigElement[i] < bigElement[k])
+                    {
+                        identicalElement = bigElement[i];
+                        bigElement[i] = bigElement[k];
+                        bigElement[k] = identicalElement;
+                    }
+                }
+            }
+            Console.WriteLine("\nНовый массив");
+            for (int l = 0; l < bigElement.Length; l++)
+            {
+                Console.Write(bigElement[l] + " ");
+
+            }
+        }
+    }
+}
